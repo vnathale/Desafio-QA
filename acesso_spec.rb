e8a23bd44833518c2c92885beb618790d1d4f65a
vnathale <vitornathale@gmail.com>
2020-10-08 20:20:44 -0300

    Add files via upload

---
+++ acesso_spec.rb
@@ -0,0 +1,15 @@
+
+describe "Controle de Acesso" do 
+
+    it "Cadastre-se com Sucesso" do 
+
+        visit "http://www.inmrobo.tk/accounts/login/"
+
+        #Cadastre-se
+
+        click_link ('nav-link text-uppercase font-weight-bold')
+        sleep 10
+
+        end
+        
+end
